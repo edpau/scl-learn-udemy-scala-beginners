ca4c5f06036b44e76f66fb252f8550fd6ad8a353
edpau <edpau@me.com>
2025-07-08 22:09:15 +0100

    feat: complete Object-Oriented Basics (Exercise)

--- src/main/scala/lectures/part2oop/OOBasics.scala
+++ src/main/scala/lectures/part2oop/OOBasics.scala
@@ -6,6 +6,18 @@ object OOBasics extends App {
   println(person.x)
   person.greet("Daniel")
   person.greet()
+
+  val author = new Writer("Charles", "Dickens", 1812)
+  val imposter = new Writer("Charles", "Dickens", 1812)
+  val novel = new Novel("Great Expectations", 1861, author)
+
+  println(novel.authorAge)
+  println(novel.isWrittenBy(imposter))
+
+  val counter = new Counter
+  counter.inc.print
+  counter.inc.inc.inc.print
+  //  counter.inc(10000).print
 }
 
 // constructor
@@ -24,6 +36,62 @@ class Person(name: String, val age: Int = 0) {
 
   // multiple constructors
   def this(name: String) = this(name, 0) // no need, as we can use default parameter
+
   def this() = this("John Doe")
 }
 
+/*
+  Novel and a Writer
+
+  Writer: first name, surname, year
+    - method fullname
+
+  Novel: name, year of release, author
+  - authorAge
+  - isWrittenBy(author)
+  - copy (new year of release) = new instance of Novel
+ */
+
+class Writer(firstName: String, surname: String, val year: Int) {
+  def fullname: String = s"$firstName $surname"
+}
+
+class Novel(name: String, year: Int, author: Writer) {
+  def authorAge: Int = year - author.year
+
+  def isWrittenBy(author: Writer) = author == this.author
+
+  def copy(newYear: Int): Novel = new Novel(name, newYear, author)
+}
+
+
+/*
+  Counter class
+    - receives an int value
+    - method current count
+    - method to increment/decrement => new Counter
+    - overload inc/dec to receive an amount
+ */
+
+class Counter(val count: Int = 0) {
+  def inc = {
+    println("incrementing")
+    new Counter(count + 1) // immutability
+  }
+
+  def dec = {
+    println("decrementing")
+    new Counter(count - 1)
+  }
+
+  def inc(n: Int): Counter = {
+    if (n <= 0) this
+    else inc.inc(n - 1)
+  }
+
+  def dec(n: Int): Counter =
+    if (n <= 0) this
+    else dec.dec(n - 1)
+
+  def print = println(count)
+}
